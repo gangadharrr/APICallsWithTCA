cc3398098fb47ace0a104f8431991dc11e1c091a
Gangadhar C <cgangadhar@presidio.com>
2024-08-15 15:41:32 +0530

    Initial Commit

---
+++ APICallsWithTCA/APICallsWithTCAApp.swift
@@ -0,0 +1,17 @@
+//
+//  APICallsWithTCAApp.swift
+//  APICallsWithTCA
+//
+//  Created by Gangadhar C on 8/15/24.
+//
+
+import SwiftUI
+
+@main
+struct APICallsWithTCAApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
